3b98b0ae21e5103de3b40d8d0881ac861e64ce83
Horsmanheimo Viktor O <viktor.horsmanheimo@helsinki.fi>
2019-03-28 13:26:03 +0200

    added postgresql support

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from os import urandom
+import os
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokemon.db"
-app.config["SQLALCHEMY_ECHO"] = True
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokemon.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -16,9 +21,7 @@ from application.pokemon import views
 from application.auth import models
 from application.auth import views
 
-
 from application.auth.models import User
-from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
@@ -28,8 +31,13 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+
+try:
+    db.create_all()
+except:
+    pass
