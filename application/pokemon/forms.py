f3506385f3073912be39af314bfd812eac371584
Horsmanheimo Viktor O <viktor.horsmanheimo@helsinki.fi>
2019-04-04 20:25:28 +0300

    added form for adding a pokemon
    the comment after the __types is for yapf formatting

--- application/pokemon/forms.py
+++ application/pokemon/forms.py
@@ -1,32 +1,47 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms import BooleanField, SelectField, validators
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from application.pokemon.models import Pokemon
+
+
+def get_pokemon():
+    return Pokemon.query.filter_by(custom=False)
+
 
 class PokeForm(FlaskForm):
-    __types = [
-        ("normal", "Normal"),
-        ("fire", "Fire"),
-        ("fighting", "Fighting"),
-        ("water", "Water"),
-        ("flying", "Flying"),
-        ("grass", "Grass"),
-        ("poison", "Poison"),
-        ("electric", "Electric"),
-        ("ground", "Ground"),
-        ("psychic", "Psychic"),
-        ("rock", "Rock"),
-        ("ice", "Ice"),
-        ("bug", "Bug"),
-        ("dragon", "Dragon"),
-        ("ghost", "Ghost"),
-        ("dark", "Dark"),
-        ("steel", "Steel")
-    ]
-
+    __types = (("normal", "Normal"),
+               ("fire", "Fire"),
+               ("fighting", "Fighting"),
+               ("water", "Water"),
+               ("flying", "Flying"),
+               ("grass", "Grass"),
+               ("poison", "Poison"),
+               ("electric", "Electric"),
+               ("ground", "Ground"),
+               ("psychic", "Psychic"),
+               ("rock", "Rock"),
+               ("ice", "Ice"),
+               ("bug", "Bug"),
+               ("dragon", "Dragon"),
+               ("ghost", "Ghost"),
+               ("dark", "Dark"),
+               ("steel", "Steel"))  # yapf: disable
     name = StringField("Pokemon name", [validators.Length(min=2, max=144)])
     description = TextAreaField("Description", [validators.Length(max=144)])
     poke_type = SelectField("type", choices=__types)
     custom = BooleanField("Custom")
+
+    class Meta:
+        csrf = False
+
+
+# TODO add like filters and shit
+class DefaultPokemonForm(FlaskForm):
+    name = QuerySelectField(
+        "Pokemon",
+        validators=[validators.Required()],
+        query_factory=get_pokemon)
 
     class Meta:
         csrf = False
