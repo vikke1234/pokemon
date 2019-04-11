cfab032e69128c9a733b9c087fb4eea1d1bbf0e4
Horsmanheimo Viktor O <viktor.horsmanheimo@helsinki.fi>
2019-04-11 19:38:48 +0300

    added account linking to Move

--- application/pokemon/models.py
+++ application/pokemon/models.py
@@ -1,6 +1,5 @@
 from application import db
 from application.models import Base
-from application.auth.models import User
 
 association_table = db.Table(
     "poke_account", Base.metadata,
@@ -19,6 +18,13 @@ pokemon_moves = db.Table(
     "pokemon_moves", Base.metadata,
     db.Column(
         "poke_id", db.Integer, db.ForeignKey("pokemon.id"), primary_key=True),
+    db.Column(
+        "move_id", db.Integer, db.ForeignKey("move.id"), primary_key=True))
+
+user_moves = db.Table(
+    "user_moves", Base.metadata,
+    db.Column(
+        "user_id", db.Integer, db.ForeignKey("account.id"), primary_key=True),
     db.Column(
         "move_id", db.Integer, db.ForeignKey("move.id"), primary_key=True))
 
@@ -52,6 +58,7 @@ class Move(Base):
     description = db.Column(db.String(255))
     pokemon = db.relationship(
         "Pokemon", backref="move", secondary=pokemon_moves)
+    account = db.relationship("User", backref="move", secondary=None)
 
     def __init__(self, name, description=None):
         self.name = name
