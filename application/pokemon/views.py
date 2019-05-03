fc1e21a043aed352396009a713e6b7c60acd9a38
Horsmanheimo Viktor O <viktor.horsmanheimo@helsinki.fi>
2019-05-03 21:44:09 +0300

    added ability to teach pokemon moves

--- application/pokemon/views.py
+++ application/pokemon/views.py
@@ -2,8 +2,9 @@ from flask import render_template, request, redirect, url_for
 from flask_login import current_user
 
 from application import app, db, login_required
-from application.pokemon.models import Pokemon
-from application.pokemon.forms import PokeForm, DefaultPokemonForm, Search
+from application.pokemon.models import Pokemon, Move
+from application.pokemon.forms import PokeForm, DefaultPokemonForm
+from application.pokemon.forms import Search, MoveForm
 from application.auth.models import User
 
 
@@ -19,42 +20,51 @@ def pokemon_index():
         "pokemon/list.html", pokemon=pokemon, search=Search())
 
 
-@app.route("/pokemon/new")
-@login_required(role="ADMIN")
-def poke_form():
-    return render_template("pokemon/new.html", form=PokeForm())
-
-
 @app.route("/pokemon/<int:poke_id>/", methods=["GET"])
 @login_required()
 def get_pokemon(poke_id):
     p = Pokemon.get_specific_pokemon(poke_id)
-    return render_template("pokemon/specific.html", pokemon=p)
+    moves = Move.query.join(Move.pokemon).filter_by(id=poke_id).join(
+        Pokemon.accounts).filter_by(id=current_user.get_id()).all()
+
+    return render_template(
+        "pokemon/specific.html", pokemon=p, moves=moves, form=MoveForm())
 
 
 @app.route("/pokemon/add_move/<int:poke_id>", methods=["POST"])
 @login_required()
 def add_move(poke_id):
-    # TODO
+    form = MoveForm(request.form)
+    move = Move.query.filter_by(name=str(form.move_name.data)).first()
     p = Pokemon.get_specific_pokemon(poke_id)
+    user = User.query.get(current_user.get_id())
+    if p not in move.pokemon:
+        move.pokemon.append(p)
+    if user not in move.account \
+       and Move.count_moves(poke_id, current_user.get_id()):
+        move.account.append(user)
 
+    db.session.add(move)
+    db.session.commit()
     return redirect(url_for("get_pokemon", poke_id=poke_id))
 
 
-@app.route("/pokemon/new", methods=["POST"])
-@login_required()
+@app.route("/pokemon/new", methods=["POST", "GET"])
+@login_required(role="ADMIN")
 def create_pokemon():
     form = PokeForm(request.form)
-    name = form.name.data
-    _type = form.poke_type.data
-    description = form.description.data
-    custom = form.custom.data
-    p = Pokemon(name, _type, description, custom)
+    if request.method == "POST" and form.validate():
+        name = form.name.data
+        _type = form.poke_type.data
+        description = form.description.data
+        custom = form.custom.data
+        p = Pokemon(name, _type, description, custom)
 
-    print("\033[93m" + "Pokemon" + "\033[0m")
-    db.session.add(p)
-    db.session.commit()
-    return redirect(url_for("pokemon_index"))
+        print("\033[93m" + "Pokemon" + "\033[0m")
+        db.session.add(p)
+        db.session.commit()
+        return redirect(url_for("pokemon_index"))
+    return render_template("pokemon/new.html", form=PokeForm())
 
 
 @app.route("/pokemon/remove/<int:poke_id>", methods=["POST", "GET"])
@@ -73,15 +83,16 @@ def remove_pokemon(poke_id):
 @login_required()
 def edit_pokemon(poke_id):
     p = Pokemon.query.get(int(poke_id))
-    if (request.method == "GET"):
-        return render_template("pokemon/edit.html", form=PokeForm(), pokemon=p)
     form = PokeForm(request.form)
-    p.name = form.name.data
-    p.poke_type = form.poke_type.data
-    p.custom = form.custom.data
-    p.description = form.description.data
-    db.session.commit()
-    return render_template("pokemon/specific.html", pokemon=p)
+    errors = None
+    if request.method == "POST" and form.validate():
+        p.name = form.name.data
+        p.poke_type = form.poke_type.data
+        p.custom = form.custom.data
+        p.description = form.description.data
+        db.session.commit()
+        return render_template("pokemon/specific.html", pokemon=p)
+    return render_template("pokemon/edit.html", form=PokeForm(), pokemon=p)
 
 
 @app.route("/pokemon/add_pokemon", methods=["GET", "POST"])
